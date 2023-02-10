b0ef49f7c2a3abe1f2a666c6acb78003e53e8801
mrvzay <vzay1999@gmail.com>
2023-02-10 07:36:14 +0530

    add some code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git repository, This is my first repository in GitHub!!");
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        int sum = 0;
+
+        while (counter <= 5) {
+            System.out.println("Enter the number #" + counter + ";");
+            String nextNumber = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(nextNumber);
+                counter++;
+                sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println("Enter the valid number!, Please try again!!.");
+            }
+        }
+        System.out.println("Sum of the numbers " + sum);
     }
 }
